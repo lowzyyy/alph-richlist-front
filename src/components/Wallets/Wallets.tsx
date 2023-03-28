c1ebed51535bd9da29a006c2f24634eff32f2c84
lowzyyy <peratheking@aol.com>
2023-03-28 14:01:31 +0200

    added onErrorRetry for alph price

--- src/components/Wallets/Wallets.tsx
+++ src/components/Wallets/Wallets.tsx
@@ -46,7 +46,20 @@ function Wallets({ pageNum, STATS_API }: Props) {
       fetch(url).then(async (res) => {
         const info = await res.json();
         return info.market_data.current_price.usd;
-      })
+      }),
+    {
+      onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        // Never retry on 404/403
+        if (error.status === 404) return;
+        if (error.status === 403) return;
+
+        // Only retry up to 3 times.
+        if (retryCount >= 3) return;
+
+        // Retry after 5 seconds.
+        setTimeout(() => revalidate({ retryCount }), 5000);
+      },
+    }
   );
   if (isLoading || isLoadingPrice) return <WalletsSkeleton></WalletsSkeleton>;
   // if (isLoading) return <WalletsSkeleton></WalletsSkeleton>;
