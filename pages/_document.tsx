ffb80a8fb39881bc2a7d423b8724574a3f8b1920
lowzyyy <peratheking@aol.com>
2023-04-19 23:59:39 +0200

    changed website description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Website for my alephium mining pool" />
+        <meta name="description" content="Richlist of Alephium crypto project" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
